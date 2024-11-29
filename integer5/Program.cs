b5e5ed7e5077fe939c1fb722c94fbff1c7f4f4ac
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 10:40:31 +0500

    lkjfdg

---
+++ integer5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace integer5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var a = int.Parse(Console.ReadLine());
+          var b = int.Parse(Console.ReadLine());
+          var result = a % b;
+          Console.WriteLine(result);
+        }
+    }
+}
